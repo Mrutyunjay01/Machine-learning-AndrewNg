00a31635392ae7d79164c9d9c461bd1a3d44f066
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-05-24 21:27:07 +0530

    element wise ./ sigmoid

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -4,7 +4,7 @@ function g = sigmoid(z)
 
 % You need to return the following variables correctly 
 g = zeros(size(z));
-
+g = 1.0 ./(1 + exp(-z)); % ./ is used for element wise operation in the matrix
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
